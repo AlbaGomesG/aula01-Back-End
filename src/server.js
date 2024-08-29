2bf2ff1714ad1693b3a9ff455f493cc5e47f2189
Giovanna Alba <giovannaalba.tds1.senai@gmail.com>
2024-08-29 15:51:35 -0300

    feat: add some requests

--- src/server.js
+++ src/server.js
@@ -9,8 +9,55 @@ const app = express()
 
 app.use(express.json())
 
+const guloseimas = [
+    {
+        id: 1,
+        nome: "Trufa",
+        preco: 8.50,
+    },
+    {
+        id: 2,
+        nome: "Jujuba",
+        preco: 10.50,
+    },
+    {
+        id: 3,
+        nome: "Brigadeirão",
+        preco: 8,
+    }
+];
+
+const filmesmarcantes = [
+    {
+        id: 1001,
+        titulo: "Avatar: o caminho da água",
+        genero: "Ficcão cientifica",
+        emCartaz: false
+    },
+    {
+        id: 1002,
+        titulo: "Toy story",
+        genero: "Animação",
+        emCartaz: false
+    },
+    {
+        id: 1003,
+        titulo: "Meu malvado favorido 4",
+        genero: "Animação",
+        emCartaz: true
+    }
+];
+
 app.get("/", (req, res) => {
     return res.status(200).send({ message: "Hello, World!"})
+})
+
+app.get("/doces", (req, res) => {
+    return res.status(200).send(guloseimas);
+})
+
+app.get("/filmes", (req, res) => {
+    return res.status(200).send(filmesmarcantes);
 })
 
 app.post("/2tds1", (req, res) => {
