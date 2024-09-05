aa27e6b405f9c135372bc6349a25e52128cf7734
Giovanna Alba <giovannaalba.tds1.senai@gmail.com>
2024-09-05 09:07:52 -0300

    feat: create routes config

--- src/server.js
+++ src/server.js
@@ -1,30 +1,17 @@
 import express from "express"
 import { config } from "dotenv"
 
+import routes from "./routes/index.routes.js";
+
 config()
 
 const port = process.env.PORT || 3000
 
-const app = express()
+const app = express();
+app.use(routes);
+app.use(express.json());
 
-app.use(express.json())
-
-
-
-
-
-app.get("/", (req, res) => {
-    return res.status(200).send({ message: "Hello, World!"})
-})
-
-
-
-
-
-app.post("/2tds1", (req, res) => {
-    return res.status(200).send({ message: "Hello, 2TDS1 - só os Devs!"})
-})
 
 app.listen(port, () => {
     console.log(`💕 Server started on http://localhost:${port}`)
-})
+});
